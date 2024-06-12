dfd8985207d3a6c9271efbf2b37ac9ef097389d4
SiddharthUptake <siddharth@uptakeinfotech.com>
2024-06-12 11:07:41 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+const mongo="mongodb+srv://Manan:Manan@mern-estate.iuodf6i.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate"
+mongoose
+  .connect(mongo)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
