6bc8d5112e172022b11c37b26511db946f8139e7
SiddharthUptake <siddharth@uptakeinfotech.com>
2024-06-12 12:27:10 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 const mongo="mongodb+srv://Manan:Manan@mern-estate.iuodf6i.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate"
+import userRouter from './routes/user.route.js';
+
 mongoose
   .connect(mongo)
   .then(() => {
@@ -16,3 +18,6 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 }
 );
+
+
+app.use('/api/user', userRouter);
